9aafcd8be7d5e0eac8547a20661a5c14a266cd2a
AJ <63066039+ajworks13@users.noreply.github.com>
2022-09-01 20:08:29 -0400

    Create Main.cs

---
+++ Main.cs
@@ -0,0 +1,48 @@
+using System; // one of many libraries. Like Import.
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// namespace cSharp Tut. // namespace is like packages in java.
+
+class Program {
+	// global
+	int number0 = 0;
+	static string hello = "Hello";
+	
+  public static void Main (string[] args) {
+		
+    Console.WriteLine ("{0}", hello);
+		Console.ReadLine(); // listens for user input.
+
+		// ------------- DATA TYPES ------------------------
+		/*
+											PRIMATIVEs
+			int : 4 bytes
+				range: (-2 31) to (2 31 - 1)
+			long : 8 bytes
+				range: (-2 63) to (2 63 - 1)
+			short : 2 bytes
+				range: (-2 15) to (2 15 - 1)
+
+			double : 8 bytes
+				range: -1.79769313486232e308
+							to 1.79769313486232e308
+			float : 4 bytes
+				range: -3.402823e38
+							to 3.42823e38
+
+
+			char : 2 bytes - a single unicode character.
+			string : 2 bytes per character.
+
+			boolean : 1 byte - true / false
+
+
+		*/
+
+		
+  } // main method / starting point.
+	
+} //class
